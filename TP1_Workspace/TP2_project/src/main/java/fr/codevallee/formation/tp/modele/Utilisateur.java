3441e38bd6ca25f3b6fbf862ada61187136aa60f
Ahmet Celikbas <celikbas.ahmet@gmail.com>
2017-08-24 16:47:46 +0200

    TP 6 terminé

--- TP1_Workspace/TP2_project/src/main/java/fr/codevallee/formation/tp/modele/Utilisateur.java
+++ TP1_Workspace/TP2_project/src/main/java/fr/codevallee/formation/tp/modele/Utilisateur.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -25,6 +26,8 @@ public class Utilisateur {
 	private String email;
 	private String password;
 	
+	@OneToOne(mappedBy="utilisateur")
+	private Telephone telephone;
 	
 	/**
 	 * @return the idUser
